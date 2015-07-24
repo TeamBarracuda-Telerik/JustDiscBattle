f4fd6adb2ce72188f1d5e3ec2d86a8bcfdfcbfc5
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-07-24 14:04:46 +0300

    Build player with properties and methods

--- Models/player.js
+++ Models/player.js
@@ -1,15 +1,47 @@
 var Player = (function () {
-    var Player = function (name, x, y, isComputer) {
+    var Player = function (name, x, y, radius, isComputer) {
         this.name = name;
         this.x = x;
         this.y = y;
+        this.radius = radius;
+        this.speed = 0;
         this.isComputer = isComputer;
         this.score = 0;
+        this.image = new Image();
+        this.image.src = './Images/player.png';
     };
 
-    function move() {
-      // Event for mouse
-    }
+    var lastMouseX = -1,
+        lastMouseY = -1;
+    Player.prototype.move = function () {
+       var self = this;
+
+        // Get mouse speed
+        if (lastMouseX > -1) {
+            this.speed = Math.max(Math.abs(self.x - lastMouseX), Math.abs(self.y - lastMouseY));
+
+        }
+
+        lastMouseX = self.x;
+        lastMouseY = self.y;
+
+        function getMousePosition(canvas, evt) {
+            var rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+        }
+        var canvas = document.getElementById('canvas-field');
+
+        canvas.addEventListener('mousemove', function(evt) {
+            var mousePos = getMousePosition(canvas, evt);
+            self.x = mousePos.x;
+            self.y = mousePos.y;
+
+        }, false);
+    };
+
 
     return Player;
 }());
