9b291c83887b467d979ef93f12dded21cf4998ad
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-07-24 14:07:52 +0300

    Build disc(puck) with properties and methods

--- Models/disc.js
+++ Models/disc.js
@@ -1,47 +1,28 @@
 var Disc = (function () {
+    var dampeningFactor = 0.99;
     var Disc = function (x, y, radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.speed = 0;
+        this.velocity = {
+            x: 0,
+            y: 0
+        };
     };
 
-    function move() {
-        var directions = {
-            upRight: {
-                x: 1,
-                y: -1
-            },
-            upLeft: {
-                x: -1,
-                y : -1
-            },
-            downRight: {
-                x: 1,
-                y: 1
-            },
-            downLeft: {
-                x : -1,
-                y : 1
-            },
-            right: {
-                x: 1,
-                y: 0
-            },
-            left: {
-                x: -1,
-                y : 0
-            },
-            up: {
-                x: 0,
-                y: -1
-            },
-            down: {
-                x : 0,
-                y : 1
-            }
-        };
-    }
+    Disc.prototype = {
+        move: function () {
+            this.velocity.x *= dampeningFactor;
+            this.velocity.y *= dampeningFactor;
+
+            // Add velocity to position
+
+            this.x += this.velocity.x;
+            this.y += this.velocity.y;
+
+        }
+    };
 
     return Disc;
 }());
