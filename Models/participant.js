d076ced54cc44c302365b23b8cba2f9439286dce
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-07-27 22:22:48 +0300

    Add abstract class Participant

---
+++ Models/participant.js
@@ -0,0 +1,24 @@
+var Participant = (function () {
+    var Participant = function (name, x, y, radius) {
+        // this makes abstract class
+        if (this.constructor === Participant) {
+            throw new Error("Can't instantiate abstract class!");
+        }
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = 0;
+        this.score = 0;
+        this.image = new Image();
+        //this.image.src = imagePath;
+    };
+
+    return Participant;
+}());
+
+// Inherit method
+Object.prototype.extends = function (parent) {
+    this.prototype = Object.create(parent.prototype);
+    this.prototype.constructor = this;
+};
