e692808e7529da2546d78dde6bff60188fac68ae
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-07-27 22:27:54 +0300

    Update game engine and implement artificial intelligence for enemy player

--- Engine/engine.js
+++ Engine/engine.js
@@ -1,8 +1,13 @@
-var canvasDrawer = new CanvasDrawer();
 SvgDrawer.svgNS = 'http://www.w3.org/2000/svg';
-var player = new Player('Pesho', 20, 180, 15);
-var disc = new Disc(canvasDrawer.canvasWidth / 2, canvasDrawer.canvasHeight / 2, 12);
-var enemy = new Enemy('Gosho', canvasDrawer.canvasWidth - 20, canvasDrawer.canvasHeight / 2, 12);
+var canvasDrawer = new CanvasDrawer(),
+    startEnemyX = canvasDrawer.canvasWidth - 20,
+    startEnemyY = canvasDrawer.canvasHeight / 2,
+    participantRadius = 15,
+    startPlayerX = 20,
+    startPlayerY = 180,
+    player = new Player('Pesho', startPlayerX, startPlayerY, participantRadius),
+    disc = new Disc(canvasDrawer.canvasWidth / 2, canvasDrawer.canvasHeight / 2, 12),
+    enemy = new Enemy('Gosho', startEnemyX, startEnemyY, participantRadius);
 
 playField('playField', 0, 0, 640, 360);
 
@@ -15,9 +20,9 @@ setInterval(runTimer, 1000);
 
 function startGame() {
     canvasDrawer.clear();
-    canvasDrawer.drawPlayer(player);
+    canvasDrawer.drawParticipant(player, "red");
+    canvasDrawer.drawParticipant(enemy, "blue");
     canvasDrawer.drawDisc(disc);
-    canvasDrawer.drawEnemy(enemy);
 
     SvgDrawer.clear(); // Clears all nodes in svg except <a> tag and it's content.
     SvgDrawer.drawForms();
@@ -28,9 +33,10 @@ function startGame() {
     disc.move();
     detectCollisionWithDisc(player, disc);
     detectCollisionWithDisc(enemy, disc);
+    detectDiscCollisionWithWalls(disc, canvasDrawer);
+    moveEnemy();
 
     function detectCollisionWithDisc(player, disc) {
-
         var dx = player.x - disc.x,
             dy = player.y - disc.y,
             distance = Math.sqrt(dx * dx + dy * dy);
@@ -47,8 +53,8 @@ function startGame() {
                 forceX = unitX * force,
                 forceY = unitY * force;
 
-            disc.velocity.x += forceX + player.speed / 2;
-            disc.velocity.y += forceY + player.speed / 2;
+            disc.velocity.x += forceX;
+            disc.velocity.y += forceY;
 
             return true;
         } else {
@@ -56,45 +62,92 @@ function startGame() {
         }
     }
 
-    // bounce off the floor
-    if (disc.y > canvasDrawer.canvasHeight - disc.radius) {
-        disc.y = canvasDrawer.canvasHeight - disc.radius;
-        disc.velocity.y = -Math.abs(disc.velocity.y);
+    function detectDiscCollisionWithWalls(disc, canvasDrawer) {
+
+        // bounce off the floor
+        if (disc.y > canvasDrawer.canvasHeight - disc.radius) {
+            disc.y = canvasDrawer.canvasHeight - disc.radius;
+            disc.velocity.y = -Math.abs(disc.velocity.y);
+        }
+
+        // bounce off ceiling
+        if (disc.y < disc.radius + 0) {
+            disc.y = disc.radius + 0;
+            disc.velocity.y = Math.abs(disc.velocity.y);
+        }
+        // bounce off right wall
+        if (disc.x > canvasDrawer.canvasWidth - disc.radius && (disc.y < 120 || 240 < disc.y)) {
+            disc.x = canvasDrawer.canvasWidth - disc.radius;
+            disc.velocity.x = -Math.abs(disc.velocity.x);
+        }
+
+        // goal in right side
+        if (disc.x >= canvasDrawer.canvasWidth && 120 < disc.y && disc.y < 240) {
+            disc.x = canvasDrawer.canvasWidth / 2;
+            disc.y = canvasDrawer.canvasHeight / 2;
+            disc.velocity.x = 0;
+            disc.velocity.y = 0;
+            player.score += 1;
+        }
+
+        // bounce off left wall
+        if (disc.x < disc.radius && (disc.y < 120 || 240 < disc.y)) {
+            disc.x = disc.radius;
+            disc.velocity.x = Math.abs(disc.velocity.x);
+        }
+
+        // goal in left side
+        if (disc.x <= 0 && 120 < disc.y && disc.y < 240) {
+            disc.x = canvasDrawer.canvasWidth / 2;
+            disc.y = canvasDrawer.canvasHeight / 2;
+            disc.velocity.x = 0;
+            disc.velocity.y = 0;
+            enemy.score += 1;
+        }
     }
 
-    // bounce off ceiling
-    if (disc.y < disc.radius + 0) {
-        disc.y = disc.radius + 0;
-        disc.velocity.y = Math.abs(disc.velocity.y);
-    }
-    // bounce off right wall
-    if (disc.x > canvasDrawer.canvasWidth - disc.radius && (disc.y < 120 || 240 < disc.y)) {
-        disc.x = canvasDrawer.canvasWidth - disc.radius;
-        disc.velocity.x = -Math.abs(disc.velocity.x);
+    // enemy movement
+    function moveEnemy() {
+        var playGroundMiddle = canvasDrawer.canvasWidth / 2,
+            enemyDefenceLine = canvasDrawer.canvasWidth * 5 / 8,
+            speed = getEnemyMoveSpeed();
+
+        if (disc.x > canvasDrawer.canvasWidth / 2) {
+            // if disc is passed defence line but not enemy x
+            if (disc.x > enemyDefenceLine && enemy.x > enemyDefenceLine) {
+                //enemy.move("left", speed.x);
+                moveEnemyX(speed.x);
+                moveEnemyY(speed.y);
+            }
+            moveEnemyY(speed.y);
+        } else {
+            enemy.reset();
+        }
     }
 
-    // goal in right side
-    if(disc.x >= canvasDrawer.canvasWidth && 120 < disc.y && disc.y < 240) {
-        disc.x = canvasDrawer.canvasWidth / 2;
-        disc.y = canvasDrawer.canvasHeight / 2;
-        disc.velocity.x = 0;
-        disc.velocity.y = 0;
-        player.score += 1;
+    function moveEnemyY(speed){
+        if (disc.y < enemy.y && disc.x < enemy.x) {
+            enemy.move("up", speed);
+        } else if (disc.y > enemy.y && disc.x < enemy.x) {
+            enemy.move("down", speed);
+        }
     }
 
-    // bounce off left wall
-    if (disc.x < disc.radius && (disc.y < 120 || 240 < disc.y)) {
-        disc.x = disc.radius;
-        disc.velocity.x = Math.abs(disc.velocity.x);
+    function moveEnemyX(speed) {
+        // if disc is passed enemy x
+        if (disc.x >= enemy.x) {
+            enemy.reset();
+        } else {
+            enemy.move("left", speed);
+        }
     }
 
-    // goal in left side
-    if(disc.x <= 0 && 120 < disc.y && disc.y < 240) {
-        disc.x = canvasDrawer.canvasWidth / 2;
-        disc.y = canvasDrawer.canvasHeight / 2;
-        disc.velocity.x = 0;
-        disc.velocity.y = 0;
-        enemy.score += 1;
+    // TODO
+    function getEnemyMoveSpeed(){
+        return {
+            x: 2.5,
+            y: 2.5
+        }
     }
 
     requestAnimationFrame(function () {
