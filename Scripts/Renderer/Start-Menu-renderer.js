1da0d3da84c93dd5a6addcb391ba749973675d4d
siderisltd <siderisltd@gmail.com>
2015-07-30 22:29:52 +0300

    Merged with start menu layout

--- Scripts/Renderer/Start-Menu-renderer.js
+++ Scripts/Renderer/Start-Menu-renderer.js
@@ -8,183 +8,183 @@ define(function () {
             /* global Kinetic */
 
             var stage = new Kinetic.Stage({
-                    container: 'start-menu-container',
-                    width: 640,
-                    height: 440
+                    container : 'start-menu-container',
+                    width     : 640,
+                    height    : 440
                 }),
+
                 svgAndCanvasContainer = document.getElementsByClassName('gameDisplayContainer')[0],
                 stageHorizontalCenter = stage.getWidth() / 2,
-                stageVerticalCenter = stage.getHeight() / 2,
-                layer = new Kinetic.Layer(),
-                buttonFill = 'purple',
-                buttonStroke = 'orange',
-                textColor = 'orange',
-                svgField = document.getElementById('svg-field');
+                stageVerticalCenter   = stage.getHeight() / 2,
+                layer                 = new Kinetic.Layer(),
+                buttonFill            = 'purple',
+                buttonStroke          = 'orange',
+                textColor             = 'orange',
+                svgField              = document.getElementById('svg-field');
 
             var startButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter - 140,
-                    y: stageVerticalCenter + 150,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter - 140,
+                    y      : stageVerticalCenter + 150,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }).rotate(10),
-
                 startButtonText = new Kinetic.Text({
-                    x: startButton.getX(),
-                    y: startButton.getY() + 2,
-                    text: 'Start',
-                    fontSize: 24,
-                    fontFamily: 'Consolas',
-                    width: startButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : startButton.getX(),
+                    y          : startButton.getY() + 2,
+                    text       : 'Start',
+                    fontSize   : 24,
+                    fontFamily : 'Consolas',
+                    width      : startButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 }).rotate(10);
 
             var tutorialButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter - 130,
-                    y: stageVerticalCenter - 35,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter - 130,
+                    y      : stageVerticalCenter - 35,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }).rotate(-10),
 
                 tutorialButtonText = new Kinetic.Text({
-                    x: tutorialButton.getX(),
-                    y: tutorialButton.getY() + 3,
-                    text: 'Tutorial',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : tutorialButton.getX(),
+                    y          : tutorialButton.getY() + 3,
+                    text       : 'Tutorial',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 }).rotate(-10);
 
-            var creditsButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter + 40,
-                    y: stageVerticalCenter - 50,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
-                }).rotate(10),
+            var creditsButton      = new Kinetic.Rect({
+                    x      : stageHorizontalCenter + 40,
+                    y      : stageVerticalCenter - 50,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
+                }).rotate(10) ,
 
-                creditsButtonText = new Kinetic.Text({
-                    x: creditsButton.getX(),
-                    y: creditsButton.getY() + 3,
-                    text: 'Credits',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                creditsButtonText  = new Kinetic.Text({
+                    x          : creditsButton.getX(),
+                    y          : creditsButton.getY() + 3,
+                    text       : 'Credits',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 }).rotate(10);
 
             var exitButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter + 140,
-                    y: stageVerticalCenter + 150,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter + 140,
+                    y      : stageVerticalCenter + 150,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }).rotate(5),
 
                 exitButtonText = new Kinetic.Text({
-                    x: exitButton.getX(),
-                    y: exitButton.getY() + 3,
-                    text: 'Back',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : exitButton.getX(),
+                    y          : exitButton.getY() + 3,
+                    text       : 'Back',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 }).rotate(5);
 
             var difficultyEasyButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter - 170,
-                    y: stageVerticalCenter - 70,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter- 50,
+                    y      : stageVerticalCenter - 80,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }),
 
                 difficultyEasyText = new Kinetic.Text({
-                    x: stageHorizontalCenter - 170,
-                    y: stageVerticalCenter - 68,
-                    text: 'Easy',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : stageHorizontalCenter- 50,
+                    y          : stageVerticalCenter - 78,
+                    text       : 'Easy',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 });
 
             var difficultyMediumButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter - 70,
-                    y: stageVerticalCenter + 30,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter- 50,
+                    y      : stageVerticalCenter - 30,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }),
 
                 difficultyMediumText = new Kinetic.Text({
-                    x: stageHorizontalCenter - 70,
-                    y: stageVerticalCenter + 32,
-                    text: 'Medium',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : stageHorizontalCenter- 50,
+                    y          : stageVerticalCenter -28,
+                    text       : 'Medium',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 });
 
             var difficultyHardButton = new Kinetic.Rect({
-                    x: stageHorizontalCenter + 30,
-                    y: stageVerticalCenter + 130,
-                    width: 100,
-                    height: 30,
-                    stroke: buttonStroke,
-                    fill: buttonFill
+                    x      : stageHorizontalCenter - 50,
+                    y      : stageVerticalCenter + 20,
+                    width  : 100,
+                    height : 30,
+                    stroke : buttonStroke,
+                    fill   : buttonFill
                 }),
 
                 difficultyHardText = new Kinetic.Text({
-                    x: stageHorizontalCenter + 30,
-                    y: stageVerticalCenter + 132,
-                    text: 'Hard',
-                    fontSize: 21,
-                    fontFamily: 'Consolas',
-                    width: tutorialButton.getWidth(),
-                    align: 'center',
-                    fill: textColor
+                    x          : stageHorizontalCenter- 50,
+                    y          : stageVerticalCenter + 22,
+                    text       : 'Hard',
+                    fontSize   : 21,
+                    fontFamily : 'Consolas',
+                    width      : tutorialButton.getWidth(),
+                    align      : 'center',
+                    fill       : textColor
                 });
 
             var tutorialText = new Kinetic.Text({
-                x: 140,
-                y: 100,
-                text: 'Welcome to Just Disc Battle!\n\n' +
-                'A, game inspired by the classic, Air hockey.' +
-                '\n Your objective is to beat the ai opponent!' +
-                '\n1 point is awarded every time a player enters the puck into the enemy player\'s goal,\n' +
+                x          : 140,
+                y          : 100,
+                text       : 'Welcome to Just Disc Battle!\n\n'+
+                'A, game inspired by the classic, Air hockey.'+
+                '\n Your objective is to beat the ai opponent!'+
+                '\n1 point is awarded every time a player enters the puck into the enemy player\'s goal,\n'+
                 'the first player that scores 15 points wins. Good luck!',
-                fontSize: 20,
-                fontFamily: 'Consolas',
-                width: 400,
-                align: 'center',
-                fill: 'white'
+                fontSize   : 20,
+                fontFamily : 'Consolas',
+                width      : 400,
+                align      : 'center',
+                fill       : 'white'
             });
 
             var creditsText = new Kinetic.Text({
-                x: 140,
-                y: 100,
-                text: 'Telerik Academy team Barracuda\n\n' + 'Authors: \n' + 'M.Yankov - Mihail Yankov\n' + 'Emiliya93 - Emilia Georghieva\n' + 'Beardhammer - Martin Ali\n' + 'Adrian.Apostolov - Adrian Apostolov\n' + 'A.sideriss - Alexis Sideriss\n' + 'enchev93 - Georgi Enchev\n' + 'dlachkov - Dimitar Lachkov\n' + 'Andrei_pl - Andrey Traykov',
-                fontSize: 20,
-                fontFamily: 'Consolas',
-                width: 400,
-                align: 'center',
-                fill: 'white'
+                x          : 140,
+                y          : 100,
+                text       : 'Telerik Academy team Barracuda\n\n' + 'Authors: \n' + 'M.Yankov - Mihail Yankov\n' + 'Emiliya93 - Emilia Georghieva\n' + 'Beardhammer - Martin Ali\n' + 'Adrian.Apostolov - Adrian Apostolov\n' + 'A.sideriss - Alexis Sideris\n' + 'enchev93 - Georgi Enchev\n' + 'dlachkov - Dimitar Lachkov\n' + 'Andrei_pl - Andrey Traykov',
+                fontSize   : 20,
+                fontFamily : 'Consolas',
+                width      : 400,
+                align      : 'center',
+                fill       : 'white'
             });
 
             function visualizeDifficultyButtons() {
@@ -246,7 +246,6 @@ define(function () {
             }
 
             function blur() {
-                //FIXME - You know why
                 svgAndCanvasContainer.className += ' blurred';
                 // attr('class', 'gameDisplayContainer blurred');
             }
@@ -272,7 +271,7 @@ define(function () {
             }
 
             function exitButtonEventHandler() {
-                // visualiseStartButton();
+                visualizeStartButton();
                 hideDifficultyButtons();
                 tutorialText.hide();
                 visualizeCreditsButton();
@@ -286,17 +285,24 @@ define(function () {
             }
 
 
-            function startTheGame() {
+            function selectDifficulty() {
                 // TODO - Optimize slightly
                 // TODO - Fade out animation
-
+                blur();
                 visualizeDifficultyButtons();
+                hideStartButton();
                 tutorialText.hide();
                 creditsText.hide();
                 hideCreditsButton();
                 hideTutorialButton();
                 visualizeExitButton();
-                unBlur();
+
+                // TODO: "Back" button needs a new position when in difficulty screen
+                // document.getElementById('start-menu-container').style.visibility = 'hidden';
+                // Theory: removing everything this way is faster, but the line above allows for a pause menu and stuff
+                // document.getElementById('start-menu-container').outerHTML = '';
+                // engine.startGame();
+
 
                 // visualizePlayField();
                 console.log('Start clicked');
@@ -311,11 +317,13 @@ define(function () {
                 // hidePlayField();
 
                 tutorialText.show();
+
+                // NOTE - BUG TEMPORARILY GONE --> Calling unBlur() here causes the start button to stop functioning
+
                 layer.draw();
             }
 
             function startCredits() {
-                // TODO - Star Wars style credits. No doubt
                 hideCreditsButton();
                 hideTutorialButton();
                 visualizeExitButton();
@@ -326,14 +334,25 @@ define(function () {
                 console.log('Credits executed');
             }
 
-            startButton.on('click', startTheGame);
-            startButtonText.on('click', startTheGame);
+            startButton.on('click', selectDifficulty);
+            startButtonText.on('click', selectDifficulty);
             tutorialButton.on('click', startTutorial);
             tutorialButtonText.on('click', startTutorial);
             creditsButton.on('click', startCredits);
             creditsButtonText.on('click', startCredits);
             exitButton.on('click', exitButtonEventHandler);
             exitButtonText.on('click', exitButtonEventHandler);
+
+            difficultyEasyButton.on('click', function(){
+
+            });
+            difficultyMediumButton.on('click', function(){
+
+            });
+            difficultyHardButton.on('click', function(){
+
+            });
+
 
             hideExitButton();
             tutorialText.hide();
